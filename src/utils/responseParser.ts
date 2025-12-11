ac6b32c4bd2df0cf55cd45e345fa659384939ab3
Aaditya Bir Singh <aadi.bir.singh@gmail.com>
2025-12-11 18:38:49 +0530

    ZOD validation implemented

--- src/utils/responseParser.ts
+++ src/utils/responseParser.ts
@@ -1,8 +1,27 @@
 import type { NarrowedContext } from "telegraf";
+import * as z from "zod";
 
 
+const llmResponseSchema = z.object({
+    intent: z.string(),
+    title: z.string(),
+    participants: z.array(z.string()),
+    date: z.string(),
+    time: z.string(),
+    reminder: z.string()
+})
 
-const responseParser = (responseText: string): JSON => {
+interface LLMResponse {
+    intent: string;
+    title: string;
+    participants: string[];
+    date: string;
+    time: string;
+    reminder: string;
+}
+
+
+const responseParser = async (responseText: string):Promise<LLMResponse>  => {
     
     const jsonMatch = responseText.match(/```json([\s\S]*?)```/);
 
@@ -15,6 +34,13 @@ const responseParser = (responseText: string): JSON => {
         textString = responseText.replace(jsonMatch[0], "").trim(); // rest of the text
     } else {
         jsonString = responseText;
+    }
+    // Validate and parse the JSON string using Zod
+    const parsed = await llmResponseSchema.safeParse(JSON.parse(jsonString));
+
+    if (!parsed.success) {
+        console.error("Invalid LLM response format:", parsed.error);
+        throw new Error("Invalid LLM response format");
     }
 
     return JSON.parse(jsonString);
